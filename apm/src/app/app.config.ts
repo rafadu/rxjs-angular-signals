9c5ff4e844bfc22925049e7b5a9548226bda50ec
Rafael Duarte <rafael.dgs.1993@gmail.com>
2025-11-23 15:33:09 -0300

    apm-begin!

--- apm/src/app/app.config.ts
+++ apm/src/app/app.config.ts
@@ -3,11 +3,14 @@ import { FormsModule } from '@angular/forms'
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppData } from './app-data';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(
       FormsModule,
+      InMemoryWebApiModule.forRoot(AppData, { delay: 1000})
     ),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
--- apm/src/app/app.routes.ts
+++ apm/src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { PageNotFoundComponent } from '../utilities/page-not-found';
 
 export const routes: Routes = [
-    { path: 'welcome', component: Home }
+    { path: 'welcome', component: Home },
+    {
+        path: 'products',
+        loadComponent: () => import('./products/product-list/product-list').then(c => c.ProductList)
+    },
+    {
+        path: 'cart',
+        loadComponent: () => import('./cart/cart-shell/cart-shell').then(c => c.CartShellComponent)
+    },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+    { path: '**', component: PageNotFoundComponent}
 ];
